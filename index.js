834e6f94fa8a450a966ab5494415e0ec8193e8ba
MinhKhangTran <minhkhang.tran@gmx.de>
2022-03-28 07:36:40 +0200

    :sparkles: add ts

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+var ratings = document.querySelectorAll(".card__rating");
+var submit = document.querySelector("button");
+var cardInit = document.querySelector(".card[data-type='normal']");
+var cardThank = document.querySelector(".card[data-type='thank you']");
+var cardNumber = document.querySelector(".card__number");
+var number;
+// add active and save value
+ratings.forEach(function (rating) {
+    rating.addEventListener("click", function (e) {
+        // delete active on other ratings
+        ratings.forEach(function (item) {
+            item.classList.remove("active");
+        });
+        e.target.classList.add("active");
+        number = Number(e.target.innerText);
+        // add acitve to submit
+        submit === null || submit === void 0 ? void 0 : submit.addEventListener("click", function () {
+            if (number === undefined) {
+                return;
+            }
+            cardInit === null || cardInit === void 0 ? void 0 : cardInit.classList.remove("active");
+            cardThank === null || cardThank === void 0 ? void 0 : cardThank.classList.add("active");
+            cardNumber.textContent = "" + number;
+        });
+    });
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("hello world");
+const ratings = document.querySelectorAll(".card__rating");
+const submit = document.querySelector("button");
+const cardInit = document.querySelector(".card[data-type='normal']");
+const cardThank = document.querySelector(".card[data-type='thank you']");
+const cardNumber = document.querySelector(".card__number");
+let number: number;
+
+// add active and save value
+ratings.forEach((rating) => {
+  rating.addEventListener("click", (e: any) => {
+    // delete active on other ratings
+    ratings.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+    number = Number(e.target.innerText);
+
+    // add acitve to submit
+    submit?.addEventListener("click", () => {
+      if (number === undefined) {
+        return;
+      }
+      cardInit?.classList.remove("active");
+      cardThank?.classList.add("active");
+      cardNumber!.textContent = "" + number;
+    });
+  });
+});
